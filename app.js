e652553a1de8ac2feee413e68636cb6246534d7c
Gricelg517 <135929737+Gricelg517@users.noreply.github.com>
2023-08-05 07:25:24 -0500

    add line 30

--- app.js
+++ app.js
@@ -8,25 +8,26 @@ const myMap = {
 	// build leaflet map
 	buildMap() {
 		this.map = L.map('map', {
-		center: this.coordinates,
-		zoom: 11,
+			center: this.coordinates,
+			zoom: 12
 		});
+
 		// add openstreetmap tiles
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution:
 			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-		minZoom: '15',
-		}).addTo(this.map)
+		minZoom: '15'
+		}).addTo(this.map);
+
 		// create and add geolocation marker
-		const marker = L.marker(this.coordinates)
-		marker
-		.addTo(this.map)
-		.bindPopup('<p1><b>You are here</b><br></p1>')
-		.openPopup()
-	},
+		const marker = L.marker(this.coordinates);
+		marker.addTo(this.map)
+			.bindPopup('<p1><b>'+ this.coordinates +'</b><br></p1>')
+			.openPopup()
+	}
 
 	// add business markers
-	addMarkers() {
+.addMarkers(){
 		for (var i = 0; i < this.businesses.length; i++) {
 		this.markers = L.marker([
 			this.businesses[i].lat,
@@ -35,24 +36,28 @@ const myMap = {
 			.bindPopup(`<p1>${this.businesses[i].name}</p1>`)
 			.addTo(this.map)
 		}
-	},
+	}
 }
 
 // get coordinates via geolocation api
-async function getCoords(){
+ async function getCoords(){
 	const pos = await new Promise((resolve, reject) => {
 		navigator.geolocation.getCurrentPosition(resolve, reject)
 	});
+
+	console.log(pos);
+
 	return [pos.coords.latitude, pos.coords.longitude]
+	
 }
 
 // get foursquare businesses
 async function getFoursquare(business) {
 	const options = {
-		method: 'GET',
-		headers: {
-		Accept: 'application/json',
-		Authorization: 'fsq3ATzZbmcGhdeFafr73wZcnJ+LlN6bK+4dh19a7ClS4u8='
+		 method: 'GET',
+		 headers: {
+		 Accept: 'application/json',
+		 Authorization: 'fsq3WEf9RCv5nIhkJcdWznIqTMsk8VjJYpQEWs0wKa9NxEM='
 		}
 	}
 	let limit = 5
@@ -77,20 +82,30 @@ function processBusinesses(data) {
 	return businesses
 }
 
-
 // event handlers
 // window load
 window.onload = async () => {
-	const coords = await getCoords()
-	myMap.coordinates = coords
-	myMap.buildMap()
+    const coords = await getCoords();
+    console.log(coords);
+    myMap.coords = coords;
+    myMap.buildMap();
+
+    // On Click Popup
+    var mapClickPopup = L.popup();
+    myMap.map.on('click', (e) => {
+        mapClickPopup.setLatLng(e.latlng)
+            .setContent(`<div class="popup" onclick="closeAllPopups()">You clicked the map at:<br>${e.latlng.toString()}</div>`)
+            .openOn(myMap.map);
+    });
 }
 
 // business submit button
 document.getElementById('submit').addEventListener('click', async (event) => {
 	event.preventDefault()
 	let business = document.getElementById('business').value
-	let data = await getFoursquare(business)
-	myMap.businesses = processBusinesses(data)
+	console.log(business);
+	let data = await getFoursquare(business);
+	console.log(data);
+	myMap.businesses = processBusinesses(data);
 	myMap.addMarkers()
 })
